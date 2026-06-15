e664ae07e1ab7b81d041f671fa3ce0ea5bd7953f
FloetteAngeFeroxSupreme <valentin.victoor@gmail.Com>
2026-06-15 13:09:56 +0200

    ex5

---
+++ ex5.py
@@ -0,0 +1,10 @@
+def syracuse(n):
+    s=[]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        s.append(n)
+    print(s)
+syracuse(int(input("entrez un nombre: ")))
